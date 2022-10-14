ac2c21a70d066cb2f5104ce5af05e2ae5660bb34
austineoduor <austineoduor57@gmail.com>
2022-10-14 17:40:27 +0300

    file

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,47 +1,49 @@
 #!/usr/bin/python3
 """Script that starts a Flask web application"""
-from flask import Flask, render_template
+from flask import Flask
 app = Flask(__name__)
+strict_slashes = False
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def hello_hbnb():
     """Method to display the welcome text"""
     return 'Hello HBNB!'
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route('/hbnb')
 def display_hbnb():
     """Method to display the welcome text"""
     return 'HBNB'
 
 
-@app.route('/c/<input>', strict_slashes=False)
-def display_c_input(input):
+@app.route('/c/<string:text>')
+def display_c_input(text):
     """Method to display C + the input text"""
-    return 'C %s' % input.replace("_", " ")
+    return 'C %s' % text.replace("_", " ")
 
 
-@app.route('/python', strict_slashes=False)
-@app.route('/python/<p_input>', strict_slashes=False)
-def display_python_input(p_input='is cool'):
+@app.route('/python/', defaults={"text":"is cool"})
+@app.route('/python')
+@app.route('/python/<string:text>')
+def display_python_input(text):
     """Method to display Python + the input text"""
-    return 'Python %s' % p_input.replace("_", " ")
+    return 'Python %s' % text.replace('_', ' ')
 
 
-@app.route('/number/<int:n>', strict_slashes=False)
+@app.route('/number/<int:n>')
 def display_number(n):
     """Method to display "is a number" in case of input n being an int"""
     return '%d is a number' % n
 
 
-@app.route('/number_template/<int:n>', strict_slashes=False)
+@app.route('/number_template/<int:n>')
 def display_template(n):
     """Method to display a template"""
     return render_template('5-number.html', n=n)
 
 
-@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+@app.route('/number_odd_or_even/<int:n>')
 def display_odd(n):
     """Method to display a template"""
     return render_template('6-number_odd_or_even.html', n=n)

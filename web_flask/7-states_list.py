d1f7e5fdc3e857e31b5e240a978e3cdd27406412
austineoduor <austineoduor57@gmail.com>
2022-10-17 04:29:00 +0300

    file

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -19,4 +19,4 @@ def display_states():
     return render_template('7-states_list.html', states=states)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000)
--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -29,4 +29,4 @@ def display_cities_by_states(id):
     return render_template('9-states.html', state=None)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000)
